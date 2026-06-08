35e3db35b4ae6cd366779c08de1b47244e6ec308
John Patrick Valencia <john.valencia@ext.essilor.com>
2026-06-09 01:19:04 +0800

    Scaffolding products with pagination and sorting

--- sb-ecom/src/main/java/com/ecommerce/project/payload/ProductResponse.java
+++ sb-ecom/src/main/java/com/ecommerce/project/payload/ProductResponse.java
@@ -9,8 +9,8 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CategoryResponse {
-    private List<CategoryDTO> content;
+public class ProductResponse {
+    private List<ProductDTO> content;
     private Integer pageNumber;
     private Integer pageSize;
     private Long totalElements;
